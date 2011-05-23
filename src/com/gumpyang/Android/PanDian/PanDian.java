e97dfa110f04fbcf06cedb4e56dd2509e218c122
Yang Cheng <yangchengwork@gmail.com>
2011-05-23 20:23:28 +0800

    可以生成数据库文件

--- src/com/gumpyang/Android/PanDian/PanDian.java
+++ src/com/gumpyang/Android/PanDian/PanDian.java
@@ -2,12 +2,17 @@ package com.gumpyang.Android.PanDian;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
 
 public class PanDian extends Activity {
+	private SQLiteDatabase mSQLiteData = null;
+	private final static String DATABASE_NAME = "test.db";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mSQLiteData = this.openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
     }
 }

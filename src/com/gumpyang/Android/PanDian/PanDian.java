36f8cd3399ab410597239f3d77fdbb03ced4062f
Yang Cheng <yangchengwork@gmail.com>
2011-05-26 20:52:06 +0800

    开始查询产品库

--- src/com/gumpyang/Android/PanDian/PanDian.java
+++ src/com/gumpyang/Android/PanDian/PanDian.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 import android.net.Uri;
 import android.content.Intent;
 import android.content.ContentValues;
@@ -43,15 +44,19 @@ public class PanDian extends Activity {
 //		+ TABLE_AMOUNT + " INTEGER)";
 	
 	private String PDATE;
+	private Cursor proCursor;
+	private Cursor panCursor;
 	
 	private Button btn_scan;
 	private Button btn_ok;
-	private TextView mBarCode;
-	private TextView mName;
-	private TextView mAmount;
+	private EditText mBarCode;
+	private EditText mName;
+	private EditText mAmount;
+	
+	private int sqlcontrol = 0; // 0 需要加入产品表和盘点表 1 产品表中有需要加入盘点表 2 盘点表需要修改数量
 	
 	private final static boolean PC_TEST = true;
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,10 +64,10 @@ public class PanDian extends Activity {
         setContentView(R.layout.main);
         
         btn_scan = (Button)findViewById(R.id.scan_bt);
-        mBarCode = (TextView)findViewById(R.id.barcode);
+        mBarCode = (EditText)findViewById(R.id.barcode);
         btn_ok = (Button)findViewById(R.id.button_ok);
-        mName = (TextView)findViewById(R.id.name);
-        mAmount = (TextView)findViewById(R.id.amount);
+        mName = (EditText)findViewById(R.id.name);
+        mAmount = (EditText)findViewById(R.id.amount);
         
         Calendar calendar = Calendar.getInstance();
         PDATE = "P" + calendar.get(Calendar.YEAR)
@@ -70,12 +75,11 @@ public class PanDian extends Activity {
         	+ calendar.get(Calendar.DAY_OF_MONTH)
         	+ calendar.get(Calendar.HOUR_OF_DAY)
         	+ calendar.get(Calendar.MINUTE);
-/*        
-        String sdcard =  "" + android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-        mName.setText(sdcard);
-*/      
+        
         // 创建或打开数据库
-        mSQLiteData = this.openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+//        String sdcard =  "" + android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+//        mName.setText(sdcard);
+        mSQLiteData = this.openOrCreateDatabase(DATABASE_NAME, MODE_WORLD_READABLE, null);
         
         // 获取数据库的cursor
         try {
@@ -100,7 +104,7 @@ public class PanDian extends Activity {
         btn_scan.setOnClickListener(new Button.OnClickListener() {
         	public void onClick(View v) {
         		if (PC_TEST) {
-        			UpdateBarcode(null); // "0123456789");
+        			UpdateBarcode("0123456789");
         		} else {
         			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
 	                intent.setPackage("com.google.zxing.client.android");
@@ -112,12 +116,18 @@ public class PanDian extends Activity {
 
         btn_ok.setOnClickListener(new Button.OnClickListener() {
         	public void onClick(View v) {
-        		mSQLiteData.close();
+        		SaveData();
         	}
         });
     }
     
-    public void UpdataAdapter() {
+	@Override
+	public void onDestroy() {
+		mSQLiteData.close();
+		super.onDestroy();
+	}
+
+	public void UpdataAdapter() {
 /**    	
     	Cursor cur = mSQLiteData.query(PRODUCT_TABLE_NAME, new String[] {TABLE_ID,
     			TABLE_NUM, TABLE_NAME, TABLE_AMOUNT}, null, null, null, null,
@@ -130,15 +140,56 @@ public class PanDian extends Activity {
     	}
 */
     }
+	
+	private void SaveData() {
+		ContentValues cv = new ContentValues();
+		switch (sqlcontrol) {
+		case 0:
+			cv.put(TABLE_NUM, "" + mBarCode.getText());
+			cv.put(TABLE_NAME, "" + mName.getText());
+			mSQLiteData.insert(PRODUCT_TABLE_NAME, null, cv);
+			cv.put(TABLE_AMOUNT, "" + mAmount.getText());
+			mSQLiteData.insert(PDATE, null, cv);
+			break;			
+		};
+		sqlcontrol = 0;
+	}
     
     private void UpdateBarcode(String num) {
     	if (num == null) {
     		mBarCode.setText(null);
     		mName.setText(null);
     		mAmount.setText(null);
+    		sqlcontrol = 0;
     		return;
     	}
         mBarCode.setText(num);
+        
+        proCursor = QueryProduct(PRODUCT_TABLE_NAME, num);
+        if (proCursor == null)
+        {
+        	mName.setText(null);
+        	mAmount.setText(null);
+        	sqlcontrol = 0;
+        	return;
+        }
+        if (panCursor == null) {
+        	sqlcontrol = 1;
+        }
+    }
+    
+    private Cursor QueryProduct(String table, String num) {
+    	Cursor mCursor = mSQLiteData.query(table, null,
+    			"num='" + num + "'", null, null, null, null);
+
+    	//mCursor不等于null,将标识指向第一条记录  
+        if (mCursor != null && mCursor.getCount() > 0) {  
+        	mAmount.setText("" + mCursor.getCount());
+            mCursor.moveToFirst();  
+        } else {
+        	mCursor = null;
+        }
+        return mCursor;  
     }
 
     /** */
